d2449f6368574d05462cc7f4b4e4c4038e8bb8b2
Santiago-Florez <66635225+Santiago-Florez@users.noreply.github.com>
2021-09-09 00:14:55 -0500

    Adelanto Clase 31/Agosto/2021

--- js/js.js
+++ js/js.js
@@ -2,6 +2,9 @@
 var rowId = 0
 /*Manejo y edicion del DOM*/
 /*obtienes elementos del HTML por su id definido*/
+/*Manejo de eventos como .onclick*/
+
+/*Mejorando el boton Guardar y Formulario*/
 document.getElementById("petsave-button").onclick = function savePet() {
 	rowId += 1
 	/*Objetos json*/
@@ -21,9 +24,10 @@ document.getElementById("petsave-button").onclick = function savePet() {
 	let tdId = document.createElement("td")
 	/*Modificacion de texto de los elementos*/
 	tdId.innerHTML = rowId
+	/*Se le agrega al elemento creado tr el otro elemento creado td*/
 	tr.appendChild(tdId)
 
-	/**/
+	/*Creacion de iterador sobre un 'arreglo'*/
 	Object.keys(info).forEach((key) => {
 		let td = document.createElement("td")
 		td.innerHTML = info[key]
@@ -31,17 +35,20 @@ document.getElementById("petsave-button").onclick = function savePet() {
 	})
 
 	let tdActions = document.createElement("td")
-	let input1 = document.createElement("input")
-	input1.setAttribute("type", "button")
-	input1.setAttribute("id", "delete-" + rowId)
-	input1.setAttribute("class", "btn btn-danger")
-	input1.value = "Eliminar"
-	input1.onclick = function(){
+	let deleteButton = document.createElement("input")
+	deleteButton.setAttribute("type", "button")
+	deleteButton.setAttribute("id", "delete-" + rowId)
+	deleteButton.setAttribute("class", "btn btn-danger")
+	deleteButton.value = "Eliminar"
+	deleteButton.onclick = function(){
+		/*Obtiene el atributo del objeto deleteButton*/
 		let id = this.getAttribute("id")
+		/*Remplaza el atrubuto del id*/
 		id = +id.replace("delete-", "")
+		/*Elimina el elemento que tengan el id que se llamo*/
 		document.getElementById("row-" + id).remove()
 	}
-	tdActions.appendChild(input1)
+	tdActions.appendChild(deleteButton)
 	let input2 = document.createElement("input")
 	input2.setAttribute("type", "button")
 	input2.setAttribute("class", "btn btn-warning")
@@ -52,3 +59,14 @@ document.getElementById("petsave-button").onclick = function savePet() {
 
 	document.getElementById("body-table").appendChild(tr) 
 };
+
+/*Modificando las imagenes de las mascotas boton mostrar Imagen*/
+document.getElementById("show-image").onclick = function(){
+	/*Documentacion del sitio web MDN Web Docs*/
+		/*llama a la API creando una promise*/
+	fetch('https://dog.ceo/api/breeds/image/random')
+		/*Indica que se debe hacer al terminar el llamado de la API, en este caso convertilo a un json*/
+		.then(response => response.json())
+		/*Indica que hacer al recivir datos del seridor en este caso imprimir lo obtenido en consola*/
+		.then(data => console.log(data));
+}
